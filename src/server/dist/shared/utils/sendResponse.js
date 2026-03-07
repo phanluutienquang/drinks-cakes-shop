37ccd7c704e49f028701a9699218d87d5de52a09
phanluutienquang <quangvip2081999@gmail.com>
2026-03-07 15:51:01 +0700

    remove env files from repo

--- src/server/dist/shared/utils/sendResponse.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sendResponse = (res, statusCode, payload) => {
-    res.status(statusCode).json(payload);
-};
-exports.default = sendResponse;
